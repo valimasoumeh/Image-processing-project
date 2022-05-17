254d841e9359c9b6295f2ad927887714ca3c72f1
Masoumeh Vali <86606514+valimasoumeh@users.noreply.github.com>
2022-05-17 17:27:10 +0200

    Add files via upload
    در این پروژه ابتدا یکسری عکس شخص با لیبل مشخص را به ماشین میدهیم و با توجه به عکس ها چهره هر فرد را encode میکنیم و encodeی صورت هر فرد را ذخیره میکنیم.
    داخل بخش تست، چهره فرد داخل بخش تست را encode میکرد و با قبلی ها که ذخیره شده بودند مقایسه میکرد و اگر از یک حدی بالاتر بودند ماشین میتوانست تشخیص دهد که عکس و صورت چه فردی هست.
    سپس فریم های وبکم را به عنوان عکس ورودی میگیریم. چون در هر ثانیه 45 فریم ذخیره میشود سپس هر فریم را که گرفتیم چک میکنیم که اصلا داخل آن فریم چهره هست یا نه. و نیز چک میکنیم که آیا آن فرد برای ماشین قبلا شناخته شده است یا نه. اگر نشناخته بود برای آن لیبل میزنیم  و encode ی چهره فرد را ذخیره میکنیم که اگر دفعه بعد آن فرد را دید تشخیص دهد. 
    
    In this project, we first give a series of photos of a person with a specific label to the machine, and according to the photos, we encode the face of each person and save the encoding of each person's face.
    Inside the test section, the person encoded the face inside the test section and compared it with the previous ones that were stored, and if they were above a certain level, the machine could identify who the photo and face were.
    Then we take the webcam frames as input photos. Because 45 frames are saved per second, then we check each frame we took to see if there is a face inside the frame at all. We also check to see if that person is already known to the car. If it is not known, we label it and save the encoding of the person's face so that if he sees that person next time, he will recognize it.

---
+++ Face Detection Using a Webcam/face/face/project.py
@@ -0,0 +1,68 @@
+import cv2
+import face_recognition
+import os
+import numpy as np
+
+# region Vars
+path = 'images_pro'
+images = []
+names = []
+myList = os.listdir(path)
+# print(myList)
+# endregion
+
+# region Fetch Known Images
+for item in myList:
+    curImage = cv2.imread(f'{path}/{item}')
+    images.append(curImage)
+    names.append(os.path.splitext(item)[0])
+# print(images)
+# print(names)
+# endregion
+
+# region Encode Known Images
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+known_encode_list = findEncodings(images)
+# print(known_encode_list[0])
+print("Encoding Completed!")
+# endregion
+
+cap = cv2.VideoCapture(0)
+
+# region Process Frames and compare them with Known Images
+while True:
+    success, frame = cap.read()
+    frame_small = cv2.resize(frame, (0,0), None, 0.25, 0.25)
+    fame_small = cv2.cvtColor(frame_small, cv2.COLOR_BGR2RGB)
+    faces_loc = face_recognition.face_locations(frame_small)
+    faces_encode = face_recognition.face_encodings(frame_small, faces_loc)
+
+    for encodeFace, faceLoc in zip(faces_encode, faces_loc):
+        matches = face_recognition.compare_faces(known_encode_list, encodeFace)
+        face_distances = face_recognition.face_distance(known_encode_list, encodeFace)
+        matchIndex = np.argmin(face_distances)
+
+        if matches[matchIndex]:
+            name = names[matchIndex].upper()
+            # print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(frame,(x1,y1), (x2,y2), (255,0,255), 2)
+            cv2.rectangle(frame, (x1,y2-35), (x2,y2), (255,0,255), cv2.FILLED)
+            cv2.putText(frame,name,
+                        (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+
+    cv2.imshow('webcam', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+# endregion
+
+cap.release()
+cv2.destroyAllWindows()
